11d3427540e55fc59c1aa782f8b8975eacdb8e37
ScrantonPete <scrantonpete@yahoo.com>
2019-05-23 23:21:05 -0400

    updated html, added main container with html elements needed for game, and additonal sound

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,5 @@
 var audio = new Audio("assets/images/bark.mp3");
 $(".btn-primary").click(() => audio.play());
+
+var audio = new Audio("assets/images/dogbark.mp3");
+$(".submitButton").click(() => audio.play());
