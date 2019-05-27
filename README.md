4c424cee5ebea1d8282ca866ad9235687bb426c3
ScrantonPete <scrantonpete@yahoo.com>
2019-05-27 12:35:34 -0400

    updated timer and readme file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TrviaGame
+
+# Dog Trivia
+
+This was the 5th homework assignment for the Boot Camp LPS. You only have 10 seconds to answer each question.
+
+#Technologies Used
+
+- HTML with Style sheet and Bootstrap
+- Javascript and jQuery
+
+#Play my game by clicking on this link:
+https://scrantonpete.github.io/TrviaGame/
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -65,14 +65,7 @@ var pictureArray = [
   "question5",
   "question6",
   "question7",
-  "question8",
-  "question9",
-  "question10",
-  "question11",
-  "question12",
-  "question13",
-  "question14",
-  "question15"
+  "question8"
 ];
 var currentQuestion;
 var correctAnswer;
@@ -141,7 +134,7 @@ function newQuestion() {
 }
 
 function countdown() {
-  seconds = 15;
+  seconds = 10;
   $("#timeLeftCounter").html("<h3>Time Remaining: " + seconds + "</h3>");
   answered = true;
   //sets timer to go down
